df93475033db0e26e5366c3d01a310cd66d16537
IvanIvanov00 <ivan.y.ivanov@protonmail.com>
2025-03-02 19:26:09 +0200

    Initial commit

---
+++ functions.py
@@ -0,0 +1,17 @@
+FILEPATH = "todos.txt"
+
+def get_todos(filepath=FILEPATH):
+    """Read a text file and return the list of to-do items."""
+
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+        # file = open('todos.txt', 'r')
+        # todos = file.readlines()
+        # file.close()
+    return todos_local
+
+
+def write_todos(todos_args, filepath=FILEPATH):
+    """ Write the to-do items list in the text file. """
+    with open(filepath, 'w') as file:
+        file.writelines(todos_args)
---
+++ main.py
@@ -0,0 +1,71 @@
+from functions import get_todos, write_todos
+import time
+
+while True:
+    # Get user input and strip space chars from it
+    user_action = input("Type add, show, edit, complete or exit: ")
+    user_action = user_action.strip()
+
+    if user_action.startswith("add"):
+        todo = user_action[4:]
+
+        todos = get_todos()
+
+        todos.append(todo + '\n')
+
+        write_todos("todos.txt", todos)
+
+    elif user_action.startswith("show"):
+        todos = get_todos()
+        # new_todos = [item.strip('\n') for item in todos] # list comprehension
+
+        for index, item in enumerate(todos):
+            item = item.strip('\n')
+            row = f"{index + 1}-{item}"
+            print(row)
+
+
+    elif user_action.startswith("edit"):
+        try:
+            number = int(user_action[5:])
+            print(number)
+
+            number = number - 1
+
+            todos = get_todos()
+
+            new_todo = input("Enter new todo: ")
+            todos[number] = new_todo + '\n'
+
+            write_todos("todos.txt", todos)
+
+        except ValueError:
+            print("Command is not valid")
+            user_action = input("Type add, show, edit, complete or exit: ")
+            user_action = user_action.strip()
+
+    elif user_action.startswith("complete"):
+        try:
+            number = int(user_action[9:])
+
+            todos = get_todos()
+
+            index = number - 1
+            todo_to_remove = todos[index].strip('\n')
+            todos.pop(index)
+
+            write_todos(filepath="todos.txt", todos_args=todos)
+
+            message = f"Todo {todo_to_remove} was removed from the list."
+            print(message)
+        except IndexError:
+            print("There is no item with that number.")
+            continue
+
+    elif user_action.startswith("exit"):
+        break
+
+    else:
+        print("Command is not valid.")
+
+print("Goodbye!")
